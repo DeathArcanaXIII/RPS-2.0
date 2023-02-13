d5ac0c02b83c1e216732c40818c096cc836af6fb
DeathArcanaXIII <120216256+DeathArcanaXIII@users.noreply.github.com>
2023-02-13 18:44:04 -0300

    Commit 11

--- scripts/Table.gd
+++ scripts/Table.gd
@@ -5,7 +5,8 @@ signal PLAYED_CARD()
 signal ENEMY_TURN_STRONGEST_PLAY()
 signal ENEMY_TURN_PAPER_LOVER()
 signal ENEMY_TURN_HOLD_JOKER()
-
+signal ENEMY_TURN_ROCK_LOVER()
+signal ENEMY_TURN_SCISSOR_LOVER()
 
 var scene_player_deck = preload("res://scenes/Player_Deck.tscn")
 var scene_enemy_deck = preload("res://scenes/Enemy_Deck.tscn")
@@ -18,22 +19,25 @@ var ai_chooser
 func _ai_chooser():
 	randomize()
 	ai_chooser = randi() % Global.AI.size()
-	print(ai_chooser)
+	
+	if(ai_chooser == Global.AI.AGRESSIVE):
+		print("Agressive")
+	elif(ai_chooser == Global.AI.PASSIVE):
+		print("Passive")
+	elif(ai_chooser == Global.AI.HOLD_JOKER):
+		print("Hold Joker")
+	elif(ai_chooser == Global.AI.PAPER_LOVER):
+		print("Paper Play")
+	elif(ai_chooser == Global.AI.ROCK_LOVER):
+		print("Rock Play")
+	elif(ai_chooser == Global.AI.SCISSOR_LOVER):
+		print("Scissors Play")
 
 func delete(card_type,node):
 	player_choice = card_type
 	Global.player_choice = player_choice
 	node.queue_free()
 	Global.player_actual_hand -= 1
-	if(ai_chooser == Global.AI.PASSIVE):
-		emit_signal("ENEMY_TURN")
-	elif(ai_chooser == Global.AI.AGRESSIVE):
-		emit_signal("ENEMY_TURN_STRONGEST_PLAY")
-	elif(ai_chooser == Global.AI.PAPER_LOVER):
-		emit_signal("ENEMY_TURN_PAPER_LOVER")
-	else:
-		emit_signal("ENEMY_TURN_HOLD_JOKER")
-
 	emit_signal("PLAYED_CARD")
 	
 func shuffle_deck(deck): #EMBARALHA A ARRAY DO DECK
@@ -88,4 +92,20 @@ func _ready():
 	$Enemy_Deck.connect("CHECK_WINNER", $Check_Winner, "check_winner")
 	$Player_Deck.connect("UPDATE_SCORE", $Check_Winner, "_emit_score")
 	$Player_Deck.connect("EMPTY_DECK", $Declare_Winner, "declare_winner")
+	pass # Replace with function body.
+
+
+func _on_Table_PLAYED_CARD():
+	if(ai_chooser == Global.AI.PASSIVE):
+		emit_signal("ENEMY_TURN")
+	elif(ai_chooser == Global.AI.AGRESSIVE):
+		emit_signal("ENEMY_TURN_STRONGEST_PLAY")
+	elif(ai_chooser == Global.AI.PAPER_LOVER):
+		emit_signal("ENEMY_TURN_PAPER_LOVER")
+	elif(ai_chooser == Global.AI.HOLD_JOKER):
+		emit_signal("ENEMY_TURN_HOLD_JOKER")
+	elif(ai_chooser == Global.AI.SCISSOR_LOVER):
+		emit_signal("ENEMY_TURN_SCISSOR_LOVER")
+	else:
+		emit_signal("ENEMY_TURN_ROCK_LOVER")
 	pass # Replace with function body.
